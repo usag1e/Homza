df2e496670effebe8d0d7be51ab48365344e7f4e
joydisee <joydisee@gmail.com>
2015-01-25 14:50:04 -0500

    Prepared couchdb support

---
+++ couchdb_handler.py
@@ -0,0 +1,75 @@
+from couchdb import *
+from couchdb.design import ViewDefinition
+
+# Creates a connection
+def connect_to_db():
+	#couch.resource.credentials = ("admin", "admin")
+	return Server('http://localhost:5984')
+
+# Creates a DB or loads it
+def create_or_load_db( couch, database_name ):
+	# Try to get the database 
+	try:
+		return couch[ database_name ]
+	# Or create it if it is not already
+	except ResourceNotFound:
+		return couch.create( database_name )
+
+def add_user( user, macs ):
+	#First you need to connect to CouchDB server
+	couch = connect_to_db()
+	#Then load a databse in the object *db*
+	db = create_or_load_db( couch, 'inhabitants' )	
+	# Let's check if the user exists already by trying to get it, and if it is not here, it means we can add it 
+	try:
+		#You can then use the object *db* to directly check a document through its _id, remember that we use urls as ids since a url is unique
+		doc = db[ user ]
+		#The object *doc* contains the document from the users databse which _id is url_rss
+		print "[couch-add_user] %s is already a user in the database" % user
+	except ResourceNotFound:
+		dict_field_values = {
+			'_id'  : user,
+			'macs'  :macs
+		}
+		#The object *db* has a create method, and this is how you create a document
+		return db.create( dict_field_values )
+
+def create_view_for_macs():
+	#First you need to connect to CouchDB server
+	couch = connect_to_db()
+	#Then load a databse in the object *db*
+	db = create_or_load_db( couch, 'inhabitants' )	
+	rpt_view = ViewDefinition(
+			'list',
+			'macs',
+			'''function(doc) {
+				for( i in doc.macs ){
+                 		emit( doc.macs[i], doc._id  );}
+			}''')
+	rpt_view.sync( db )
+
+def retrieve_user_for_mac( mac ):
+	#First you need to connect to CouchDB server
+	couch = connect_to_db()
+	#Then load a databse in the object *db*
+	db = create_or_load_db( couch, 'inhabitants' )	
+	for row in db.view( "_design/list/_view/macs" ):
+		if mac == row.key:
+			print
+			print mac, row.id
+	return row.id
+
+def update_last_seen_time( user, time ):
+	
+	#First you need to connect to CouchDB server
+	couch = connect_to_db()
+	#Then load a databse in the object *db*
+	db = create_or_load_db( couch, 'inhabitants' )	
+
+	doc = db[ user ]
+	doc['last_seen_time'] = time
+
+	db.save(doc)
+
+
+
---
+++ init_users.py
@@ -0,0 +1,34 @@
+#You absolutely need these 2 imports at the beginning of your script to be able to use these functions, also you mest be in the same folder
+from couchdb_handler import *
+import couchdb
+
+
+#Create a new user
+user = "ilyass"
+macs = ["8C:3A:E3:3E:56:26"]
+#The function add_user checks if the user is already existing in the database
+add_user( user, macs )
+
+user = "joe"
+macs = ["BC:77:37:E7:0B:49", "34:FC:EF:CB:A1:BB" ]
+add_user( user, macs )
+
+user = "Lussylver"
+macs = ["98:D6:F7:6A:52:13", "80:56:F2:70:85:DB" ]
+add_user( user, macs )
+
+
+
+#Creates a view in CouchDB which allows us to use retrieve_user_for_mac( mac 
+create_view_for_macs( )
+
+
+
+# Retrieve the user corresponding to a MAC address
+mac = "8C:3A:E3:3E:56:26"
+retrieve_user_for_mac( mac )
+
+
+#Update a user's last_seen_time
+update_last_seen_time( "ilyass", "now" )
+
