d465dfff7e3b02faa5c36df48886daf74574d0be
joydisee <joydisee@gmail.com>
2015-03-21 00:53:40 -0400

    Added Couchdb integration for Octopi API

--- octopi_test.py
+++ octopi_test.py
@@ -2,42 +2,35 @@ import requests
 from pprint import pprint
 import json
 
+def retrieve_printer_status():
+	status = {}
 
-r = requests.get('http://192.168.1.156/api/connection', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
-print r.url
-content = r.content
-data = json.loads(content)
-print "Printer Status:"
-status = data['current']['state']
-print data['current']['state']
+	r = requests.get('http://192.168.1.156/api/connection', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
+	content = r.content
+	data = json.loads(content)
+	printer_status = data['current']['state']
+	status['printer_status'] = printer_status
 
+	r = requests.get('http://192.168.1.156/api/printer', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
+	content = r.content
+	data = json.loads(content)
+	bed_temperature = data['temps']['bed']['actual']
+	status['bed_temperature'] = bed_temperature
+	head_temperature = data['temps']['tool0']['actual']
+	status['head_temperature'] = head_temperature
 
-r = requests.get('http://192.168.1.156/api/printer', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
-print r.url
-content = r.content
-data = json.loads(content)
-bed_temperature = data['temps']['bed']['actual']
-print "Bed temperature:"
-print bed_temperature
-head_temperature = data['temps']['tool0']['actual']
-print "Head temperature:"
-print head_temperature
+	payload = {'state': 'true'}
+	r = requests.get('http://192.168.1.156/api/job', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
+	content = r.content
+	data = json.loads(content)
+	job_name = data['job']['file']['name']
+	status['job_name'] = job_name	
+	completion = data['progress']['completion']
+	status['completion'] = completion
+	elapsed_print_time = data['progress']['printTime']
+	status['elapsed_print_time'] = elapsed_print_time
+	remaining_print_time = data['progress']['printTimeLeft']
+	status['remaining_print_time'] = remaining_print_time
+	return status
 
-payload = {'state': 'true'}
-r = requests.get('http://192.168.1.156/api/job', auth=('API', '3D2318DE50E14E42AC2C804EC5DA4B81') )
-print r.url
-content = r.content
-data = json.loads(content)
-job_name = data['job']['file']['name']
-print "Job name:"
-print job_name
-completion = data['progress']['completion']
-print "Completion:"
-print completion
-elapsed_print_time = data['progress']['printTime']
-print "Elapsed print time:"
-print elapsed_print_time
-remaining_print_time = data['progress']['printTimeLeft']
-print "Remaining print time:"
-print remaining_print_time
 
