3432f91a126f3b4610d50311399f046f69b5faa6
Ilyass <joydisee@gmail.com>
2015-10-25 21:49:09 -0400

    Repaired weather API by adding key

--- weather_test.py
+++ weather_test.py
@@ -7,8 +7,9 @@ import time
 def getWeather():
 	status= {}
 	# Requesting weather from OpenWeatherMap for Montreal, in .json
-	r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&mode=json&units=metric' % ( 45.50, -73.56 ) )
+	r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&mode=json&units=metric&APPID=b55dc015063b76e0ba8531ffa35d7df6' % ( 45.50, -73.56 ) )
     #extracting the .json data from the request
+	print r
 	rj = r.json()
     # Printing the whole .json thing, with pprint() 'cause they say it's nicer
     #print rj
