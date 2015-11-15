7c866e9a675c3cfc048ada7fa62f4ae8730c23c7
usag1e <jo_wax@hotmail.com>
2015-11-15 10:33:01 -0500

    Added try except on weather

--- weather.py
+++ weather.py
@@ -5,16 +5,17 @@ import time
 
 # Function to get the weather in Montreal
 def getWeather():
-	status= {}
-	# Requesting weather from OpenWeatherMap for Montreal, in .json
+    status= {}
+    # Requesting weather from OpenWeatherMap for Montreal, in .json
+    try:
 	r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&mode=json&units=metric&APPID=b55dc015063b76e0ba8531ffa35d7df6' % ( 45.50, -73.56 ) )
     #extracting the .json data from the request
 	print r
 	rj = r.json()
-    # Printing the whole .json thing, with pprint() 'cause they say it's nicer
-    #print rj
-	#pprint(rj)
-    # Temperature is called 'temp' and indented in 'main', we retrieve it
+        # Printing the whole .json thing, with pprint() 'cause they say it's nicer
+        #print rj
+        #pprint(rj)
+        # Temperature is called 'temp' and indented in 'main', we retrieve it
 	pressure = rj['main']['pressure']
 	temperature = rj['main']['temp']
         icon = rj['weather'][0]['icon']
@@ -25,20 +26,20 @@ def getWeather():
 	wind = rj['wind']['speed']
 
 	sunrise = (
-    datetime.datetime.fromtimestamp(
-        int(SR)
-    ).strftime('%Y-%m-%d %H:%M:%S')
-)
+    	    datetime.datetime.fromtimestamp(
+        	int(SR)
+    	    ).strftime('%Y-%m-%d %H:%M:%S')
+	)
 	sunset = (
-    datetime.datetime.fromtimestamp(
-        int(SS)
-    ).strftime('%Y-%m-%d %H:%M:%S')
-)
+	    datetime.datetime.fromtimestamp(
+		int(SS)
+	    ).strftime('%Y-%m-%d %H:%M:%S')
+	)
 	time_of_treatment = (
-    datetime.datetime.fromtimestamp(
-        int(time.mktime(time.localtime()))
-    ).strftime('%Y-%m-%d %H:%M:%S')
-)
+	    datetime.datetime.fromtimestamp(
+		int(time.mktime(time.localtime()))
+	    ).strftime('%Y-%m-%d %H:%M:%S')
+	)
 	print time_of_treatment
 
 	status['temperature'] = temperature
@@ -55,6 +56,8 @@ def getWeather():
 	print '[Weather] The temperature is:', temperature ,'C'
 	print '[Weather] The pressure is:', pressure
 	print '[Weather] The current state is:', state
+	return status
+    except:
 	return status
 
 
