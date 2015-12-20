c5fc7da3df4a88cc4ee0a435d8f20ef5cc4549f0
joydisee <joydisee@gmail.com>
2015-12-20 13:36:14 -0500

    First commit

---
+++ internet.py
@@ -0,0 +1,20 @@
+import urllib2 
+import logging
+
+logger = logging.getLogger(__name__)
+
+class internet():
+
+    def __init__(self):
+        self.checkConnection()
+    
+    
+    def checkConnection(self):
+        try:
+            urllib2.urlopen("http://www.google.com").close()
+        except urllib2.URLError:
+	    logger.warning("[internet] OFF")
+            self.internet = False
+        else:
+	    logger.info("[internet] ON")
+            self.internet = True
