e26a8f0de451f3c60d60d085005e4320b771278d
joydisee <joydisee@gmail.com>
2015-12-20 17:57:58 -0500

    Created house import parameters

---
+++ iss.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 16:36:07 2015
+
+@author: ilyass
+"""
+
+class iss_finder:
+    
+    def __init__():
+        
+    def import_house():
+        script_dir = os.path.dirname(__file__)
+        
