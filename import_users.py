216fd663f0dbe891524cc5a3044f9851257611d6
Adrien Thiery <adrien.thiery@gmail.com>
2015-12-20 16:21:04 -0500

    import user script ok

---
+++ import_users.py
@@ -0,0 +1,19 @@
+import yaml, os
+from models import User
+
+def get_users():
+    script_dir = os.path.dirname(__file__)
+    try:
+        with open(os.path.join(script_dir, "users.yml"), 'r') as ymlfile:
+            users = yaml.load(ymlfile)
+            return users
+    except Exception as e:
+        raise e
+
+users = get_users()
+for user in users:
+    User(user.name, user.macs, user.image, user.song)
+
+#Creates a view in CouchDB which allows us to use retrieve_user_for_mac( mac 
+# create_view_for_macs( )
+# create_view_for_house()
