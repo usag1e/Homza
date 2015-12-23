e72a04a0ca4f04f9f2c8feacaf98b7a2a754b8db
Adrien Thiery <adrien.thiery@gmail.com>
2015-12-23 16:29:43 -0500

    Still a small problem with image

--- interface/src/components/user.vue
+++ interface/src/components/user.vue
@@ -1,23 +1,27 @@
 <style>
-.user {
-  flex: 1;
-  width: 200px;
-  height: 200px;
-}
+  .user {
+    flex: 1;
+    width: 200px;
+    height: 200px;
+  }
 </style>
 
 <template>
-<div
-  class='user'
-  :class='{ here: isHere }'
-  title='{{ name }}'
-  style='background: url({{ image }});'
->
-</div>
+  <div
+    class='user'
+    :class='{ here: isHere }'
+    title='{{ name }}'
+    style='background-image: url({{ image }});'
+  >
+  </div>
 </template>
 
 <script>
-exports.default = {
-  props: ['name', 'image', 'isHere']
-}
+  exports.default = {
+    props: [
+      'name',
+      'image',
+      'isHere'
+    ]
+  }
 </script>
